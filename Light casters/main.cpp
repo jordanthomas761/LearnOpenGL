3879c8cf745366b8149c87ebd7c887504984336d
Jordan Thomas <jordanthomas761@gmail.com>
2019-11-04 08:55:52 -0500

    Changed to certificates to sign locally

---
+++ Light casters/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "camera.h"
+
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
